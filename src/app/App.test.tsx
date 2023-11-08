cbe23f7e5c6870f8fb392526dbe166637eb20ed1
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-11-08 23:00:12 +0300

    chore: setup mocking http requests

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -3,10 +3,11 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 describe('App', () => {
-  it('renders', () => {
+  it('renders', async () => {
     render(<App />);
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
       'Search for Products of DummyJSON'
     );
+    expect(await screen.findByText('mock test')).toBeInTheDocument();
   });
 });
