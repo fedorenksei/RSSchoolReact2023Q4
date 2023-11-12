41cfd319b314bc338310a59e3adab13291d93985
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-11-12 23:18:26 +0300

    test: add tests for Details

--- src/tests/test-cases/details.test.tsx
+++ src/tests/test-cases/details.test.tsx
@@ -1,13 +1,46 @@
-import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { describe, expect, test } from 'vitest';
+import { Details } from '../../widgets/Details';
 import { products } from '../server/mock-data';
 import { arrangeProduct } from './utils';
 
 describe('Details', () => {
-  // see in Card --> Check that a loading indicator is displayed while fetching data;
-  // see in Card --> Make sure the detailed card component correctly displays the detailed card data;
+  test('7.1: loading indicator is displayed while fetching data', async () => {
+    const product = products[0];
+    const { actionClick } = arrangeProduct(product);
 
-  test('Ensure that clicking the close button hides the component.', async () => {
+    expect(screen.queryByTestId('loader')).toBeNull();
+
+    await actionClick();
+
+    await screen.findByTestId('loader');
+    await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
+  });
+
+  test('7.2: Detailed card component correctly displays the detailed card data', async () => {
+    const product = products[0];
+    const router = createMemoryRouter(
+      [{ element: <Details />, path: '/details/:detailsId' }],
+      {
+        initialEntries: [`/details/${product.id}`],
+      }
+    );
+    render(<RouterProvider router={router}></RouterProvider>);
+    await screen.findByTestId('loader');
+    await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
+
+    const img = screen.getByRole('img') as HTMLImageElement;
+    expect(img.src).toBe(product.thumbnail);
+    screen.getByText(product.title);
+    screen.getByText(product.description);
+  });
+
+  test('7.3: Ensure that clicking the close button hides the component.', async () => {
     const product = products[0];
     const { actionClick, router, user } = arrangeProduct(product);
 
