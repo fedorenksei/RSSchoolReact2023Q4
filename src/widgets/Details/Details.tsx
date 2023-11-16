561181b2864b4543133d4c8f7ca03e42eef51907
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-11-16 23:19:05 +0300

    feat: add isLoading slice for product details

--- src/widgets/Details/Details.tsx
+++ src/widgets/Details/Details.tsx
@@ -1,13 +1,21 @@
 import { Link, useParams } from 'react-router-dom';
-import { Product } from '../entities/Product';
-import { useGetProductByIdQuery } from '../shared/external/rtk-query';
-import { Loader } from '../shared/ui-kit/Loader';
+import { Product } from '../../entities/Product';
+import { useGetProductByIdQuery } from '../../shared/external/rtk-query';
+import { Loader } from '../../shared/ui-kit/Loader';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setIsLoading } from './details-slice';
 
 export const Details = () => {
+  const dispatch = useDispatch();
   const { detailsId } = useParams();
   const { data, isError, isLoading, isFetching } = useGetProductByIdQuery(
     detailsId as string
   );
+
+  useEffect(() => {
+    dispatch(setIsLoading(isLoading || isFetching));
+  }, [isLoading, isFetching, dispatch]);
 
   let searchResults;
   if (isError) searchResults = <p>Something went wrong...</p>;
