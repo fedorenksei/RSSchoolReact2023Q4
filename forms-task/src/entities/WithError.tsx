86638375fb107e94320982ca6574c70253f555ee
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-12-04 03:35:23 +0400

    feat: display password strength

--- forms-task/src/entities/WithError.tsx
+++ forms-task/src/entities/WithError.tsx
@@ -1,4 +1,5 @@
 import { Collapsible } from '@/shared/ui-kit/Collapsible';
+import clsx from 'clsx';
 import { PropsWithChildren, useEffect, useRef } from 'react';
 
 interface Props extends PropsWithChildren {
@@ -15,9 +16,27 @@ export const WithErrors = ({ errors, children, fieldName }: Props) => {
 
   const allErrors = Array.from(new Set(errors.concat(prevErrors.current)));
 
+  const passwordWeakness = errors.filter(
+    (err) => err.indexOf('enter') === -1
+  ).length;
+
   return (
     <div>
       {children}
+      {fieldName === 'password' && errors.length === passwordWeakness && (
+        <p
+          className={clsx(
+            {
+              1: 'text-yellow-500',
+              2: 'text-amber-600',
+              3: 'text-orange-600',
+              0: 'text-green-600',
+            }[passwordWeakness]
+          )}
+        >
+          Your password strength is {4 - passwordWeakness} of 4
+        </p>
+      )}
       <div className="text-red-400">
         <Collapsible isOpen={allErrors.length > 0}>
           {allErrors.map((err) => (
