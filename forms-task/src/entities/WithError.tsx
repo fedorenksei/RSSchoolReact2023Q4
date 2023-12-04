21396c38296e0d654b673565748b35265b354d60
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-12-04 13:32:52 +0400

    fix: don't show password strength on the start

--- forms-task/src/entities/WithError.tsx
+++ forms-task/src/entities/WithError.tsx
@@ -1,6 +1,6 @@
 import { Collapsible } from '@/shared/ui-kit/Collapsible';
 import clsx from 'clsx';
-import { PropsWithChildren, useEffect, useRef } from 'react';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 
 interface Props extends PropsWithChildren {
   errors: string[];
@@ -9,9 +9,11 @@ interface Props extends PropsWithChildren {
 
 export const WithErrors = ({ errors, children, fieldName }: Props) => {
   const prevErrors = useRef<string[]>([]);
+  const [isPasswordDirty, setIsPasswordDirty] = useState(false);
 
   useEffect(() => {
     prevErrors.current = errors;
+    if (errors.length) setIsPasswordDirty(true);
   }, [errors]);
 
   const allErrors = Array.from(new Set(errors.concat(prevErrors.current)));
@@ -23,20 +25,22 @@ export const WithErrors = ({ errors, children, fieldName }: Props) => {
   return (
     <div>
       {children}
-      {fieldName === 'password' && errors.length === passwordWeakness && (
-        <p
-          className={clsx(
-            {
-              1: 'text-yellow-500',
-              2: 'text-amber-600',
-              3: 'text-orange-600',
-              0: 'text-green-600',
-            }[passwordWeakness]
-          )}
-        >
-          Your password strength is {4 - passwordWeakness} of 4
-        </p>
-      )}
+      {fieldName === 'password' &&
+        errors.length === passwordWeakness &&
+        isPasswordDirty && (
+          <p
+            className={clsx(
+              {
+                1: 'text-yellow-500',
+                2: 'text-amber-600',
+                3: 'text-orange-600',
+                0: 'text-green-600',
+              }[passwordWeakness]
+            )}
+          >
+            Your password strength is {4 - passwordWeakness} of 4
+          </p>
+        )}
       <div className="text-red-400">
         <Collapsible isOpen={allErrors.length > 0}>
           {allErrors.map((err) => (
